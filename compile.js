972c020f06dc2de8f3df2a3329417b2d785454b8
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2023-10-11 05:12:27 +0530

    [compile] Inject `version` metadata from `package.json` at compile time

--- compile.js
+++ compile.js
@@ -2,17 +2,21 @@
 const sass = require('sass');
 const path = require('node:path');
 const fs = require('node:fs');
+const project = require('./package.json');
 
 // Files
 const inputFile = path.join('src', 'gmail.scss');
 const outputFile = path.join('dist', 'gmail.user.css');
 
 // Compilation
-const compiledScss = sass.compile(inputFile);
+const compiledCSS = sass
+    .compile(inputFile)
+    .css
+    .replace('$VAR_VERSION$', project.version);
 
 // Output
 try {
-    fs.writeFileSync(outputFile, compiledScss.css);
+    fs.writeFileSync(outputFile, compiledCSS);
     console.log('Output written to "dist" directory.');
 } catch (error) {
     console.error(`Failed to compile SCSS: \n ${error}`);
